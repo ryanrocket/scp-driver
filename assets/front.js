17a7250342904c35d6a7dbac9bfc7f368ae49e71
Ryan <ryan@ryanwans.com>
2021-09-16 18:54:59 -0400

    Automated Git Message

--- assets/front.js
+++ assets/front.js
@@ -3,7 +3,7 @@
 
     setTimeout(() => {
         $("#loading_overlay").css("visibility", "hidden")
-    }, 1)
+    }, 0)
 
     setInterval(update_time, 1000);
 
@@ -14,4 +14,29 @@
 
 function update_time() {
     $('#time').html(moment().format('HH:mm:ss a'));
+}
+
+window.master = {
+    status: (target, type, inner) => {
+        $("#"+target).attr("status", type)
+        $("#"+target).html(inner)
+    },
+    svgUpdate: (arr, color) => {
+        var colors = {
+            green: "#ff25ff",
+            grey: "#666666",
+            red: "#0dbed9"
+        };
+        var arrows = {
+            stb: "arr2",
+            mtb: "arr4",
+            btm: "arr3",
+            stm: "arr1"
+        };
+        var targetArrow = arrows[arr];
+        var targetColor = colors[color];
+        $("#"+targetArrow+"-1").attr("stroke", targetColor)
+        $("#"+targetArrow+"-2").attr("stroke", targetColor)
+        $("#"+targetArrow+"-2").attr("fill", targetColor)
+    }
 }
